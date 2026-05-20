6b51d0ca01f12671822bd7f835ea3444fbdd0798
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2026-05-20 14:18:13 +0700

    add credential user login

---
+++ data/users.ts
@@ -0,0 +1,9 @@
+export const admin = {
+  email: 'admin@practicesoftwaretesting.com',
+  password: 'welcome01'
+}
+
+export const user = {
+  email: 'customer@practicesoftwaretesting.com',
+  password: 'welcome01'
+}
